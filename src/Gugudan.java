1ba32c126c3af272ef80ce979e968d70982213f7
jieun <plaintype@gmail.com>
2023-04-02 15:34:19 +0900

    배열로 구구단 구현

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -28,19 +28,46 @@ public class Gugudan {
 //	}
 		
 	// 8,9단
-	System.out.println("구구단 중 출력할 단은? : ");
-	Scanner scanner = new Scanner(System.in);
-	int number = scanner.nextInt();
-	System.out.println("사용자가 입력한 값 : " + number);
+//	System.out.println("구구단 중 출력할 단은? : ");
+//	Scanner scanner = new Scanner(System.in);
+//	int number = scanner.nextInt();
+//	System.out.println("사용자가 입력한 값 : " + number);
+//	
+//	if(number < 2) {
+//		System.out.println("값을 잘못 입력했습니다.");
+//	} else if(number > 9) {
+//		System.out.println("값을 잘못 입력했습니다.");
+//	} else {
+//		for(int i = 1; i< 10; i++) {
+//			System.out.println(number * i);
+//		}
+//	}
+		
+	int[] result = new int[9];
+	for(int i = 0; i < result.length; i++) {
+		result[i] = 2 * (i + 1);
+	}
 	
-	if(number < 2) {
-		System.out.println("값을 잘못 입력했습니다.");
-	} else if(number > 9) {
-		System.out.println("값을 잘못 입력했습니다.");
-	} else {
-		for(int i = 1; i< 10; i++) {
-			System.out.println(number * i);
-		}
+	for(int i = 0; i < result.length; i++) {
+		System.out.println(result[i]);
+	}
+	
+	result = new int[9]; // 기존에 있던 9개의 값 덮어 씌우는 것.
+	for(int i = 0; i < result.length; i++) {
+		result[i] = 3 * (i + 1);
+	}
+	
+	for(int i = 0; i < result.length; i++) {
+		System.out.println(result[i]);
+	}
+	
+	int[] times4 = new int[9]; // 기존에 있던 9개의 값 덮어 씌우는 것.
+	for(int i = 0; i < times4.length; i++) {
+		times4[i] = 4 * (i + 1);
+	}
+	
+	for(int i = 0; i < times4.length; i++) {
+		System.out.println(times4[i]);
 	}
 	
 
